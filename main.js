3d739d7de828f2bd0f123ba307084b082d08a18e
Shankar Weldmesh Pvt. Ltd <77322849+ManitKedia@users.noreply.github.com>
2021-10-27 14:21:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 video = "";
+status = "";
+objects = [];
 
 function preload()
 {
@@ -15,6 +17,9 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 500, 450);
+    if(status != "") {
+        objectDetector.detect(video, gotResults);
+    }
 }
 
 function start()
@@ -30,4 +35,24 @@ function modelLoaded()
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResults(error, results)
+{
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+    for(i = 0; i < objects.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        document.getElementById("number_of_objects").innerHTML = "No. of objects detected are : " + objects.length;
+
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[1].x + 15, objects[1].y + 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[1].x, objects[1].y, objects[1].width, objects[1].height);
+    }
 }
